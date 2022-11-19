4a4e56dafcd3a2a826365af342aec2666481c5b7
wcocuzza <wallace.cocuzza@gmail.com>
2022-11-19 19:36:51 -0300

    Adicionado interface e adicionado diretivas ao todolist

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+//Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+
+  public taskList: Array<TaskList> = [
+    // { task: "Minha nova task", checked: true },
+    // { task: "Minha nova task2", checked: false },
+
+  ];
 
   constructor() { }
 
