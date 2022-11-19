732b5d3eee0ca3c444065c1983e6b940b792d1dc
wcocuzza <wallace.cocuzza@gmail.com>
2022-11-19 20:47:28 -0300

    Adicionado as funções para os botões de delete

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -11,14 +11,22 @@ import { TaskList } from '../../model/task-list';
 export class TodoListComponent implements OnInit {
 
   public taskList: Array<TaskList> = [
-    // { task: "Minha nova task", checked: true },
-    // { task: "Minha nova task2", checked: false },
-
+    { task: "Minha nova task", checked: true },
+    { task: "Minha nova task2", checked: false },
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Você deseja realmente excluir tudo?");
+    if (confirm) this.taskList = [];
   }
 
 }
