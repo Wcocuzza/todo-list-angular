24fb05676ec2e9631172f5e5c3a3c86b9312b7a8
wcocuzza <wallace.cocuzza@gmail.com>
2022-11-19 21:34:50 -0300

    Desenvolvido a validação da task eo sort para as task finalizadas

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 //Interface
 import { TaskList } from '../../model/task-list';
@@ -8,13 +8,14 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
   public taskList: Array<TaskList> = [];
 
-  constructor() { }
+  constructor() { };
 
-  ngOnInit(): void {
+  ngDoCheck(): void {
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
   }
 
   public setEmitTaskList(event: string) {
@@ -31,6 +32,15 @@ export class TodoListComponent implements OnInit {
   public deleteAllTaskList() {
     const confirm = window.confirm("Você deseja realmente excluir tudo?");
     if (confirm) this.taskList = [];
+  }
+
+  public validationInput(event: string, index: number) {
+    if( event.length) {
+      const confirm = window.confirm("Task está vazia, deseja excluir?");
+      if(confirm) {
+        this.deleteItemTaskList(index);
+      }
+    }
   }
 
 }
