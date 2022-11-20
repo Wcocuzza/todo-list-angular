b81f63ce45ab3cde980d5c59d0dcf9a693c46bca
wcocuzza <wallace.cocuzza@gmail.com>
2022-11-19 21:13:20 -0300

    adicionado as funções para criar tasks

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoInputAddItensComponent implements OnInit {
 
+  @Output() public emmitItemTaskList = new EventEmitter();
+
+  public additemTaskList: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public submitItemTaskList() {
+    this.additemTaskList = this.additemTaskList.trim();
+    if (this.additemTaskList) {
+      this.emmitItemTaskList.emit(this.additemTaskList);
+      this.additemTaskList = '';
+    };
+  };
 
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -10,15 +10,19 @@ import { TaskList } from '../../model/task-list';
 })
 export class TodoListComponent implements OnInit {
 
-  public taskList: Array<TaskList> = [
-    { task: "Minha nova task", checked: true },
-    { task: "Minha nova task2", checked: false },
-  ];
+  public taskList: Array<TaskList> = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public setEmitTaskList(event: string) {
+    this.taskList.push({
+      task: event,
+      checked: false
+    });
+  };
 
   public deleteItemTaskList(event: number) {
     this.taskList.splice(event, 1);
